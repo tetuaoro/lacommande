eb3cd2983087e1f8e2d4853f14689e20c77cb32b
tetuaoro <tetuaoropro@gmail.com>
2020-07-24 13:11:34 -1000

    Upd : first page

---
+++ src/Controller/AppController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/", name="app_")
+ */
+class AppController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     * @Route("/accueil", name="home")
+     */
+    public function index()
+    {
+        return $this->render('app/index.html.twig', [
+            'controller_name' => 'AppController',
+        ]);
+    }
+}
