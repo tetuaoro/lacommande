ce41fe0da5c2b49b73069f74f0b3ed2bd1e6f06e
tetuaoro <tetuaoropro@gmail.com>
2020-09-09 10:55:01 -1000

    Test : #4 test timezone

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -47,6 +47,7 @@ class ContactController extends AbstractController
         return $this->render('contact/new.html.twig', [
             'contact' => $contact,
             'form' => $form->createView(),
+            'tz' => date_default_timezone_get()
         ]);
     }
 
