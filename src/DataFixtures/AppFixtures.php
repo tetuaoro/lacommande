2a873174efd209aefdd3b4c4353c1e0afda36241
tetuaoro <tetuaoropro@gmail.com>
2020-10-07 14:22:50 -1000

    Fix : fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -12,11 +12,11 @@ class AppFixtures extends Fixture
     {
         $notif = (new Notification())
             ->setTitle('Bienvenue')
-            ->setMessage(`<p>Iaorana,</p>
+            ->setMessage("<p>Iaorana,</p>
         <p></p>
         <p>Arii Food vous souhaite la bienvenue et vous remercie d'avoir choisi nos services. Jusqu'en février 2021, la plateforme sera gratuite et nous permettra d'essayer la configuration en place afin de répondre à vos besoin. Nous restons à votre disposition via notre mail professionnel : support@ariifood.pf.</p>
         <p></p>
-        <p>Arii Food,</p>`)
+        <p>Arii Food,</p>")
         ;
 
         $manager->persist($notif);
