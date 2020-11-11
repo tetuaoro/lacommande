e3faf34c58b7d73e60c9b0f3d2e0617a46c67d1f
tetuaoro <tetuaoropro@gmail.com>
2020-11-10 14:44:11 -1000

    Upd : sign mail & set env

--- src/MessageHandler/SendEmailMessageHandler.php
+++ src/MessageHandler/SendEmailMessageHandler.php
@@ -6,8 +6,16 @@ use App\Message\SendEmailMessage;
 use App\Repository\CommandRepository;
 use App\Repository\UserRepository;
 use App\Service\Mailer;
+use Symfony\Bridge\Twig\Extension\RoutingExtension;
+use Symfony\Bridge\Twig\Mime\BodyRenderer;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Symfony\Component\Mime\Crypto\SMimeSigner;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Twig\Environment;
+use Twig\Extra\CssInliner\CssInlinerExtension;
+use Twig\Extra\Inky\InkyExtension;
+use Twig\Loader\FilesystemLoader;
 
 final class SendEmailMessageHandler implements MessageHandlerInterface
 {
@@ -15,6 +23,8 @@ final class SendEmailMessageHandler implements MessageHandlerInterface
     private $userRepo;
     private $commandRepo;
     private $mailerInterface;
+    private $twigBodyRenderer;
+    private $signer;
 
     // https://symfonycasts.com/screencast/mailer/route-context#why-is-the-link-broken
     // always 'localhost' with url()
@@ -23,24 +33,43 @@ final class SendEmailMessageHandler implements MessageHandlerInterface
         Mailer $mailer,
         UserRepository $userRepository,
         CommandRepository $commandRepository,
-        MailerInterface $mailerInterface
+        MailerInterface $mailerInterface,
+        UrlGeneratorInterface $urlGeneratorInterface,
+        $templateFolder,
+        $crt,
+        $key,
+        $passphrase
     ) {
         $this->mailer = $mailer;
         $this->userRepo = $userRepository;
         $this->commandRepo = $commandRepository;
         $this->mailerInterface = $mailerInterface;
+
+        // =====
+
+        $twigEnv = (new Environment(new FilesystemLoader($templateFolder)));
+        $twigEnv->addExtension(new InkyExtension());
+        $twigEnv->addExtension(new CssInlinerExtension());
+        $twigEnv->addExtension(new RoutingExtension($urlGeneratorInterface));
+
+        $this->signer = new SMimeSigner($crt, $key, $passphrase);
+        $this->twigBodyRenderer = new BodyRenderer($twigEnv);
     }
 
     public function __invoke(SendEmailMessage $message)
     {
         $mode = $message->getMode();
-        
+        $mail = '';
+
         if (1 == $mode) {
-            $this->mailerInterface->send($this->mailer->sendConfirmationNewUser($this->userRepo->find($message->getUser())));
+            $mail = $this->mailer->sendConfirmationNewUser($this->userRepo->find($message->getUser()));
         } elseif (2 == $mode) {
-            $this->mailerInterface->send($this->mailer->sendCommand($this->commandRepo->find($message->getCommand())));
+            $mail = $this->mailer->sendCommand($this->commandRepo->find($message->getCommand()));
         } elseif (3 == $mode) {
-            $this->mailerInterface->send($this->mailer->validateCommand($this->commandRepo->find($message->getCommand()), $message->getBool(), $this->userRepo->find($message->getUser())));
+            $mail = $this->mailer->validateCommand($this->commandRepo->find($message->getCommand()), $message->getBool(), $this->userRepo->find($message->getUser()));
         }
+
+        $this->twigBodyRenderer->render($mail);
+        $this->mailerInterface->send($this->signer->sign($mail));
     }
 }
